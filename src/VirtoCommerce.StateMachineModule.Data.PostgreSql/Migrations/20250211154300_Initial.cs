db0a56be3465d122564a4c4a75b6ede56144dd2a
Denis Vasichkin <denis.vasichkin@gmail.com>
2025-06-13 13:04:11 +0300

    fix: PSQL Initial migration

--- src/VirtoCommerce.StateMachineModule.Data.PostgreSql/Migrations/20250211154300_Initial.cs
+++ src/VirtoCommerce.StateMachineModule.Data.PostgreSql/Migrations/20250211154300_Initial.cs
@@ -7,59 +7,52 @@ namespace VirtoCommerce.StateMachineModule.Data.PostgreSql.Migrations
     /// <inheritdoc />
     public partial class Initial : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            var createStateMachineDefinitionTableScript = @"
-                CREATE TABLE IF NOT EXISTS StateMachineDefinition (
-                    Id character varying(128) NOT NULL PRIMARY KEY,
-                    Name character varying(512) NULL,
-                    EntityType character varying(128) NOT NULL, 
-                    IsActive boolean NOT NULL,
-                    Version character varying(32) NOT NULL,
-                    StatesSerialized text NULL,
-                    CreatedDate timestamp with time zone NOT NULL,
-                    ModifiedDate timestamp with time zone NULL,
-                    CreatedBy character varying(64) NULL,
-                    ModifiedBy character varying(64) NULL
-                );";
+            var ddl = @"
+            CREATE TABLE IF NOT EXISTS ""StateMachineDefinition"" (
+                ""Id"" varchar(128) NOT NULL DEFAULT gen_random_uuid(),
+                ""Name"" varchar(512) NULL,
+                ""EntityType"" varchar(128) NOT NULL,
+                ""IsActive"" boolean NOT NULL,
+                ""Version"" varchar(32) NOT NULL,
+                ""StatesSerialized"" text NULL,
+                ""CreatedDate"" timestamp with time zone NOT NULL,
+                ""ModifiedDate"" timestamp with time zone NULL,
+                ""CreatedBy"" varchar(64) NULL,
+                ""ModifiedBy"" varchar(64) NULL,
+                PRIMARY KEY(""Id"")
+            );
 
-            migrationBuilder.Sql(createStateMachineDefinitionTableScript);
+            CREATE TABLE IF NOT EXISTS ""StateMachineInstance"" (
+                ""Id"" varchar(128) NOT NULL DEFAULT gen_random_uuid(),
+                ""EntityId"" varchar(512) NULL,
+                ""EntityType"" varchar(128) NOT NULL,
+                ""StateMachineId"" varchar(128) NOT NULL,
+                ""State"" varchar(128) NOT NULL,
+                ""CreatedDate"" timestamp with time zone NOT NULL,
+                ""ModifiedDate"" timestamp with time zone NULL,
+                ""CreatedBy"" varchar(64) NULL,
+                ""ModifiedBy"" varchar(64) NULL,
+                PRIMARY KEY(""Id""),
+                CONSTRAINT ""FK_StateMachineInstance_StateMachineDefinition_StateMachineId""
+                FOREIGN KEY(""StateMachineId"")
+                REFERENCES ""StateMachineDefinition""(""Id"")
+                ON DELETE CASCADE
+            );
 
-            var createStateMachineInstanceTableScript = @"
-                CREATE TABLE IF NOT EXISTS StateMachineInstance (
-                    Id character varying(128) NOT NULL PRIMARY KEY,
-                    EntityId character varying(512) NULL,
-                    EntityType character varying(128) NOT NULL, 
-                    StateMachineId character varying(128) NOT NULL, 
-                    State character varying(128) NOT NULL, 
-                    CreatedDate timestamp with time zone NOT NULL,
-                    ModifiedDate timestamp with time zone NULL,
-                    CreatedBy character varying(64) NULL,
-                    ModifiedBy character varying(64) NULL,
-                    CONSTRAINT FK_StateMachineInstance_StateMachineDefinition_StateMachineId
-                    FOREIGN KEY (StateMachineId)
-                    REFERENCES StateMachineDefinition(Id)
-                    ON DELETE CASCADE
-                );";
-
-            migrationBuilder.Sql(createStateMachineInstanceTableScript);
-
-            var createStateMachineInstanceIndexScript = @"
-                CREATE INDEX IF NOT EXISTS IX_StateMachineInstance_StateMachineId
-                ON StateMachineInstance (StateMachineId);";
-
-            migrationBuilder.Sql(createStateMachineInstanceIndexScript);
+            CREATE INDEX IF NOT EXISTS ""IX_StateMachineInstance_StateMachineId""
+                ON ""StateMachineInstance""(""StateMachineId"");
+            ";
+            migrationBuilder.Sql(ddl);
         }
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "StateMachineInstance");
-
-            migrationBuilder.DropTable(
-                name: "StateMachineDefinition");
+            migrationBuilder.Sql(@"
+            DROP TABLE IF EXISTS ""StateMachineInstance"";
+            DROP TABLE IF EXISTS ""StateMachineDefinition"";
+            ");
         }
     }
 }
